65904f5079d14905fcd5dab536d7ad9bca8caf45
car5753 <car5753@naver.com>
2026-05-26 18:38:10 +0900

    5/26 박민상 폴더생성

---
+++ Assets/Readme.txt.cs
@@ -0,0 +1,48 @@
+/*
+Assets
+├─ script
+│  ├─ map
+│  ├─ character
+│  ├─ post
+│  ├─ item
+│  ├─ inventory
+│  ├─ crafting
+│  ├─ building
+│  ├─ combat
+│  ├─ gathering
+│  ├─ level
+│  ├─ monster
+│  ├─ npc
+│  ├─ ai
+│  ├─ ui
+│  ├─ save
+│  ├─ sound
+│  ├─ scene
+│  ├─ system
+│  └─ interface
+│
+├─ prefab
+│  ├─ character
+│  ├─ monster
+│  ├─ npc
+│  ├─ item
+│  ├─ resource
+│  ├─ building
+│  ├─ projectile
+│  ├─ effect
+│  ├─ ui
+│  └─ map
+│
+├─ animation
+│  ├─ scene
+│  ├─ action
+│  ├─ character
+│  ├─ monster
+│  ├─ npc
+│  ├─ ui
+│  └─ effect
+
+
+폴더 구조 만들어놨는데 필요없거나 이상하면 삭제하시고 새로 만드셔요
+    - 박민상
+*/
